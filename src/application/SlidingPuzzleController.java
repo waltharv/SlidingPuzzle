bf58de74389dbcea626bfa28c7637ac3fe35c59e
user <walt.harv@gmail.com>
2015-08-05 06:03:38 -0700

    Added more license stuff.

--- src/application/SlidingPuzzleController.java
+++ src/application/SlidingPuzzleController.java
@@ -1,9 +1,20 @@
-/* Walter Harvey
- * CIS494 - Java
- * 4/12/15
- * Final Project - Slider Puzzle
- * 
- */
+// Sliding Puzzle game
+// Copyright (C) 2015  Walter Harvey
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 
 package application;
  
@@ -18,7 +29,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
-
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
